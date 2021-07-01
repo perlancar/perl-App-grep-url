a38676d1b4c9114c88b5be1dcdfe8ec4d5b2bdd6
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-07-01 20:31:51 +0700

    Finish first version

--- lib/App/grep/url.pm
+++ lib/App/grep/url.pm
@@ -1,4 +1,4 @@
-package App::abgrep;
+package App::grep::url;
 
 # AUTHORITY
 # DATE
@@ -16,7 +16,7 @@ use Perinci::Sub::Util qw(gen_modified_sub);
 our %SPEC;
 
 gen_modified_sub(
-    output_name => 'abgrep',
+    output_name => 'grep_url',
     base_name   => 'AppBase::Grep::grep',
     summary     => 'Print lines having URL(s) (optionally of certain criteria) in them',
     description => <<'_',
@@ -48,6 +48,19 @@ _
             tags => ['category:url-criteria'],
         },
 
+        scheme_contains => {
+            schema => 'str*',
+            tags => ['category:url-criteria'],
+        },
+        scheme_not_contains => {
+            schema => 'str*',
+            tags => ['category:url-criteria'],
+        },
+        scheme_matches => {
+            schema => 're*',
+            tags => ['category:url-criteria'],
+        },
+
         host_contains => {
             schema => 'str*',
             tags => ['category:url-criteria'],
@@ -58,14 +71,6 @@ _
         },
         host_matches => {
             schema => 're*',
-            tags => ['category:url-criteria'],
-        },
-        host_is => {
-            schema => 'str*',
-            tags => ['category:url-criteria'],
-        },
-        host_not => {
-            schema => 'str*',
             tags => ['category:url-criteria'],
         },
 
@@ -82,13 +87,24 @@ _
             tags => ['category:url-criteria'],
         },
 
-        # XXX more criteria against query param
+        query_param_contains => {
+            schema => ['hash*', of=>'str*'],
+            tags => ['category:url-criteria'],
+        },
+        query_param_not_contains => {
+            schema => ['hash*', of=>'str*'],
+            tags => ['category:url-criteria'],
+        },
+        query_param_matches => {
+            schema => ['hash*', of=>'str*'], # XXX of re
+            tags => ['category:url-criteria'],
+        },
 
         files => {
             'x.name.is_plural' => 1,
             'x.name.singular' => 'file',
             schema => ['array*', of=>'filename*'],
-            pos => 1,
+            pos => 0,
             slurpy => 1,
         },
 
@@ -99,14 +115,21 @@ _
         $meta->{examples} = [
             {
                 summary => 'Show lines that contain at least 2 URLs',
-                'src' => q([[prog]] --min-urls 2),
+                'src' => q([[prog]] --min-urls 2 file.txt),
                 'src_plang' => 'bash',
                 'test' => 0,
                 'x.doc.show_result' => 0,
             },
             {
                 summary => 'Show lines that contain URLs from google',
-                'src' => q([[prog]] --host-matches google),
+                'src' => q([[prog]] --host-contains google file.txt),
+                'src_plang' => 'bash',
+                'test' => 0,
+                'x.doc.show_result' => 0,
+            },
+            {
+                summary => 'Show lines that contain search URLs from google where query contains the keyword "mortal"',
+                'src' => q([[prog]] --host-contains google --query-param-contains q=mortal file.txt),
                 'src_plang' => 'bash',
                 'test' => 0,
                 'x.doc.show_result' => 0,
@@ -117,10 +140,7 @@ _
         my %args = @_;
         my ($fh, $file);
 
-        my @files = @{ $args{files} // [] };
-        if ($args{regexps} && @{ $args{regexps} }) {
-            unshift @files, delete $args{pattern};
-        }
+        my @files = @{ delete($args{files}) // [] };
 
         my $show_label = 0;
         if (!@files) {
@@ -155,13 +175,14 @@ _
 
         require Regexp::Pattern::URI;
         require URI;
+        require URI::QueryParam;
 
         my @re;
         for my $scheme (@{ $args{schemes} // [] }) {
-            if    ($scheme eq 'ftp')  { push @re, $Regexp::Pattern::URI::RE{ftp} }
-            elsif ($scheme eq 'http') { push @re, $Regexp::Pattern::URI::RE{http} }
-            elsif ($scheme eq 'ssh')  { push @re, $Regexp::Pattern::URI::RE{ssh} }
-            elsif ($scheme eq 'file') { push @re, $Regexp::Pattern::URI::RE{file} }
+            if    ($scheme eq 'ftp')  { push @re, $Regexp::Pattern::URI::RE{ftp}{pat} }
+            elsif ($scheme eq 'http') { push @re, $Regexp::Pattern::URI::RE{http}{pat} }
+            elsif ($scheme eq 'ssh')  { push @re, $Regexp::Pattern::URI::RE{ssh}{pat} }
+            elsif ($scheme eq 'file') { push @re, $Regexp::Pattern::URI::RE{file}{pat} }
             else { die "grep-url: Unknown URL scheme '$scheme'\n" }
         }
         die "grep-url: Please add one or more schemes\n" unless @re;
@@ -182,7 +203,123 @@ _
             return 1 unless @urls;
             for (@urls) { $_ = URI->new($_) }
 
-            1;
+            my $match = 0;
+          URL:
+            for my $url (@urls) {
+
+                # scheme criteria
+                if (defined $fargs->{scheme_contains}) {
+                    if ($fargs->{ignore_case} ?
+                         index(lc($url->scheme), lc($fargs->{scheme_contains})) >= 0 :
+                         index($url->scheme    , $fargs->{scheme_contains})     >= 0) {
+                    } else {
+                        next;
+                    }
+                }
+                if (defined $fargs->{scheme_not_contains}) {
+                    if ($fargs->{ignore_case} ?
+                         index(lc($url->scheme), lc($fargs->{scheme_not_contains})) < 0 :
+                         index($url->scheme    , $fargs->{scheme_not_contains})     < 0) {
+                    } else {
+                        next;
+                    }
+                }
+                if (defined $fargs->{scheme_matches}) {
+                    if ($fargs->{ignore_case} ?
+                            $url->scheme =~ qr/$fargs->{scheme_matches}/i :
+                            $url->scheme =~ qr/$fargs->{scheme_matches}/) {
+                    } else {
+                        next;
+                    }
+                }
+
+                # host criteria
+                if (defined $fargs->{host_contains}) {
+                    if ($fargs->{ignore_case} ?
+                         index(lc($url->host), lc($fargs->{host_contains})) >= 0 :
+                         index($url->host    , $fargs->{host_contains})     >= 0) {
+                    } else {
+                        next;
+                    }
+                }
+                if (defined $fargs->{host_not_contains}) {
+                    if ($fargs->{ignore_case} ?
+                         index(lc($url->host), lc($fargs->{host_not_contains})) < 0 :
+                         index($url->host    , $fargs->{host_not_contains})     < 0) {
+                    } else {
+                        next;
+                    }
+                }
+                if (defined $fargs->{host_matches}) {
+                    if ($fargs->{ignore_case} ?
+                            $url->host =~ qr/$fargs->{host_matches}/i :
+                            $url->host =~ qr/$fargs->{host_matches}/) {
+                    } else {
+                        next;
+                    }
+                }
+
+                # path criteria
+                if (defined $fargs->{path_contains}) {
+                    if ($fargs->{ignore_case} ?
+                         index(lc($url->path), lc($fargs->{path_contains})) >= 0 :
+                         index($url->path    , $fargs->{path_contains})     >= 0) {
+                    } else {
+                        next;
+                    }
+                }
+                if (defined $fargs->{path_not_contains}) {
+                    if ($fargs->{ignore_case} ?
+                         index(lc($url->path), lc($fargs->{path_not_contains})) < 0 :
+                         index($url->path    , $fargs->{path_not_contains})     < 0) {
+                    } else {
+                        next;
+                    }
+                }
+                if (defined $fargs->{path_matches}) {
+                    if ($fargs->{ignore_case} ?
+                            $url->path =~ qr/$fargs->{path_matches}/i :
+                            $url->path =~ qr/$fargs->{path_matches}/) {
+                    } else {
+                        next;
+                    }
+                }
+
+                # query param criteria
+                if (defined $fargs->{query_param_contains}) {
+                    for my $param (keys %{ $fargs->{query_param_contains} }) {
+                        if ($fargs->{ignore_case} ?
+                                index((lc($url->query_param($param)) // ''), lc($fargs->{query_param_contains}{$param})) >= 0 :
+                                index(($url->query_param($param)  // '')   , $fargs->{query_param_contains}{$param})     >= 0) {
+                        } else {
+                            next URL;
+                        }
+                    }
+                }
+                if (defined $fargs->{query_param_not_contains}) {
+                    for my $param (keys %{ $fargs->{query_param_not_contains} }) {
+                        if ($fargs->{ignore_case} ?
+                                index((lc($url->query_param($param)) // ''), lc($fargs->{query_param_not_contains}{$param})) < 0 :
+                                index(($url->query_param($param) // '')    , $fargs->{query_param_not_contains}{$param})     < 0) {
+                        } else {
+                            next URL;
+                        }
+                    }
+                }
+                if (defined $fargs->{query_param_matches}) {
+                    for my $param (keys %{ $fargs->{query_param_matches} }) {
+                        if ($fargs->{ignore_case} ?
+                                ($url->query_param($param) // '') =~ qr/$fargs->{query_param_matches}{$param}/i :
+                                ($url->query_param($param) // '') =~ qr/$fargs->{query_param_matches}{$param}/) {
+                        } else {
+                            next URL;
+                        }
+                    }
+                }
+
+                $match++; last;
+            }
+            $match;
         };
 
         AppBase::Grep::grep(%args);
